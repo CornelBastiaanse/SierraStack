a291abe1a286845ab7603b19e3e53a5ab292f57c
Cornel-Bastiaanse <168725821+Cornel-Bastiaanse@users.noreply.github.com>
2025-05-04 13:31:12 +0200

    Added performance behavior unit tests

--- SierraStack.Mediator.Tests/Behaviors/PerformanceBehaviorTests.cs
+++ SierraStack.Mediator.Tests/Behaviors/PerformanceBehaviorTests.cs
@@ -1,6 +1,32 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SierraStack.Mediator.Behaviors.Performance;
+
 namespace SierraStack.Mediator.Tests.Behaviors;
 
 public class PerformanceBehaviorTests
 {
-    
+    [Fact]
+    public async Task Should_Execute_And_Log_Performance()
+    {
+        var logger = new LoggerFactory().CreateLogger<PerformanceBehavior<object, string>>();
+        var options = Options.Create(new PerformanceBehaviorOptions
+        {
+            WarningThresholdMilliseconds = 1,
+            LogEveryRequest = true
+        });
+
+        var behavior = new PerformanceBehavior<object, string>(logger, options);
+
+        var result = await behavior.HandleAsync(
+            new object(),
+            CancellationToken.None, 
+            async () =>
+            {
+                await Task.Delay(10);
+                return "OK";
+            });
+
+        Assert.Equal("OK", result);
+    }
 }
