af04b73a0be1d6793de5d7c74f4f70aec52200e7
Cornel-Bastiaanse <168725821+Cornel-Bastiaanse@users.noreply.github.com>
2025-05-04 13:31:53 +0200

    Added exception handling behavior unit tests

--- SierraStack.Mediator.Tests/Behaviors/ExceptionHandlingBehaviorTests.cs
+++ SierraStack.Mediator.Tests/Behaviors/ExceptionHandlingBehaviorTests.cs
@@ -1,6 +1,22 @@
+using Microsoft.Extensions.Logging;
+using SierraStack.Mediator.Behaviors.ExceptionHandling;
+
 namespace SierraStack.Mediator.Tests.Behaviors;
 
 public class ExceptionHandlingBehaviorTests
 {
-    
+    [Fact]
+    public async Task Should_Log_And_Rethrow_Exception()
+    {
+        var logger = new LoggerFactory().CreateLogger<ExceptionHandlingBehavior<object, string>>();
+        var behavior = new ExceptionHandlingBehavior<object, string>(logger);
+
+        var ex = await Assert.ThrowsAsync<InvalidOperationException>(() =>
+            behavior.HandleAsync(
+                new object(),
+                CancellationToken.None, 
+                () => throw new InvalidOperationException("Oops")));
+
+        Assert.Equal("Oops", ex.Message);
+    }
 }
