b7bf99ccfba8f80585874a1104f8681ce2e160f8
Cornel-Bastiaanse <168725821+Cornel-Bastiaanse@users.noreply.github.com>
2025-05-04 13:33:12 +0200

    Added caching behavior unit tests

--- SierraStack.Mediator.Tests/Behaviors/ValidationBehaviorTests.cs
+++ SierraStack.Mediator.Tests/Behaviors/ValidationBehaviorTests.cs
@@ -1,6 +1,28 @@
+using Microsoft.Extensions.Caching.Memory;
+using SierraStack.Mediator.Behaviors.Caching;
+
 namespace SierraStack.Mediator.Tests.Behaviors;
 
 public class ValidationBehaviorTests
 {
-    
+    [Fact]
+    public async Task Should_Cache_Response()
+    {
+        var cache = new MemoryCache(new MemoryCacheOptions());
+        var request = new SampleCacheableRequest();
+
+        var behavior = new CachingBehavior<SampleCacheableRequest, string>(cache);
+
+        var first = await behavior.HandleAsync(request, CancellationToken.None, () => Task.FromResult("result-1"));
+        var second = await behavior.HandleAsync(request, CancellationToken.None, () => Task.FromResult("result-2"));
+
+        Assert.Equal("result-1", first);
+        Assert.Equal("result-1", second); // Should use cached
+    }
+
+    private class SampleCacheableRequest : ICacheableRequest<string>
+    {
+        public string CacheKey => "test-key";
+        public TimeSpan? AbsoluteExpirationRelativeToNow => TimeSpan.FromMinutes(5);
+    }
 }
